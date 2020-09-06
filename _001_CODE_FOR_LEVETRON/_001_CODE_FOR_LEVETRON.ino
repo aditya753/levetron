fd5359293b0b041707a5a7bffcf19eef8f669664
aditya753 <68152621+aditya753@users.noreply.github.com>
2020-09-06 07:12:39 +0530

    Add files via upload

---
+++ _001_CODE_FOR_LEVETRON/_001_CODE_FOR_LEVETRON.ino
@@ -0,0 +1,23 @@
+int x=0;
+void setup(){
+  pinMode(A0,INPUT);
+  Serial.begin(9600);
+  pinMode(4,OUTPUT);
+  pinMode(13,OUTPUT);
+  
+}
+void loop(){
+int x=analogRead(A0);
+Serial.println(x);
+delay(6);
+digitalWrite(13,HIGH);
+if(x<100)
+digitalWrite(4,HIGH);
+delay(10);
+digitalWrite(13,HIGH);
+
+if(x>100);
+digitalWrite(4,LOW);
+}
+
+
